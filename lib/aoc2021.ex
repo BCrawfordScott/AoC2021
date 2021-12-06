9ceb350bea287ef260ab642b88ee2a457796e524
BCrawfordScott <briancrawfordscott@gmail.com>
2021-12-05 16:26:17 -0800

    Refactor solve to be dynamic for all solutions

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -3,32 +3,28 @@ defmodule Aoc2021 do
   Compiled solutions for Advent of Code 2021: https://adventofcode.com/2021
   """
 
-  alias Aoc2021.Day1
-  alias Aoc2021.Day2
-  alias Aoc2021.Day3
-  alias Aoc2021.Day4
-  alias Aoc2021.Day5
-
   def solve do
-    # Day 1
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 1,", :light_yellow, " part",  :light_white, " 1: ", :light_green, "#{Day1.refactor_part1()}"])
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 1,", :light_yellow, " part",  :light_white, " 2: ", :light_green, "#{Day1.refactor_part2()}"])
-    IO.puts ""
-    # Day 2
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 2,", :light_yellow, " part",  :light_white, " 1: ", :light_green, "#{Day2.part1()}"])
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 2,", :light_yellow, " part",  :light_white, " 2: ", :light_green, "#{Day2.part2()}"])
-    IO.puts ""
-    # Day 3
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 3,", :light_yellow, " part",  :light_white, " 1: ", :light_green, "#{Day3.part1()}"])
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 3,", :light_yellow, " part",  :light_white, " 2: ", :light_green, "#{Day3.part2()}"])
-    IO.puts ""
-    # Day 4
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 4,", :light_yellow, " part",  :light_white, " 1: ", :light_green, "#{Day4.part1()}"])
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 4,", :light_yellow, " part",  :light_white, " 2: ", :light_green, "#{Day4.part2()}"])
-    IO.puts ""
-    # Day 5
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 5,", :light_yellow, " part",  :light_white, " 1: ", :light_green, "#{Day5.part1()}"])
-    IO.puts IO.ANSI.format([:light_cyan, "Day",  :light_white, " 5,", :light_yellow, " part",  :light_white, " 2: ", :light_green, "#{Day5.part2()}"])
-    IO.puts ""
+    run()
+  end
+
+  defp run do
+    1..25
+    |> Enum.each(fn day ->
+      {module, _} = Code.eval_string("#{__MODULE__}.Day#{day}")
+      case Code.ensure_loaded(module) do
+        {:module, loaded_module} -> 1..2 |> Enum.each(fn part -> solution_for(loaded_module, day, part) end)
+        {:error, _} -> IO.puts IO.ANSI.format([:light_red, "Solutions for Day#{day} not yet available."])
+      end
+      IO.puts ""
+    end)
+  end
+
+  defp solution_for(module, day, part) do
+    case function_exported?(module, String.to_atom("part#{part}"), 0) do
+      true ->
+        solution = apply(module, String.to_atom("part#{part}"), [])
+        IO.puts IO.ANSI.format([:black_background, :light_cyan, "Day",  :light_white, " #{day},", :light_yellow, " part",  :light_white, " #{part}: ", :light_green, "#{solution}"])
+      false -> IO.puts IO.ANSI.format([:light_red, "Solutions for Day #{day} part #{part} not yet available."])
+    end
   end
 end
--- lib/aoc_2021/day1/day1.ex
+++ lib/aoc_2021/day1/day1.ex
@@ -3,38 +3,17 @@ defmodule Aoc2021.Day1 do
   @input_file Path.dirname(__ENV__.file) <> "/input.txt"
   # @test_file Path.dirname(__ENV__.file) <> "/test.txt"
 
-  # First attempt, naive solution
   def part1 do
-    # IO.puts @input_file
     @input_file
-    |> Input.to_array()
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce({ nil, 0 }, &(track_increases(&1, &2)))
-    |> total_increases()
+    |> parse_input()
+    |> compare(1)
   end
 
   def part2 do
     @input_file
-    |> Input.to_array()
-    |> Enum.map(&String.to_integer/1)
-    |> reduce_by_window
-    |> Enum.reduce({ nil, 0 }, &(track_increases(&1, &2)))
-    |> total_increases()
+    |> parse_input()
+    |> compare(3)
   end
-
-  defp track_increases(depth, {nil, _}), do: { depth, 0 }
-  defp track_increases(depth, {last_depth, num_increases}) when depth > last_depth, do: { depth, num_increases + 1 }
-  defp track_increases(depth, {_last_depth, num_increases}), do: { depth, num_increases }
-
-  defp total_increases({ _depth, num_increases }), do: num_increases
-
-  defp reduce_by_window([a | [b | [c | []]]]), do: [a + b + c]
-  defp reduce_by_window([a | [b | [c | rest]]]), do: [a + b + c | reduce_by_window([b | [c | rest]])]
-
-
-  # Second attempt after studying other solutions:
-  def refactor_part1, do: compare(1)
-  def refactor_part2, do: compare(3)
 
   defp parse_input(path) do
     path
@@ -42,8 +21,7 @@ defmodule Aoc2021.Day1 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp compare(offset) do
-    list = parse_input(@input_file)
+  defp compare(list, offset) do
     {increases, _} = list |> Enum.reduce({[], 0}, fn depth, {acc, idx} ->
       if depth < Enum.at(list, idx + offset, 0), do: {[1|acc], idx + 1}, else: {[0|acc], idx + 1}
     end)
