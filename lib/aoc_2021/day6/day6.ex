040cc1fd74865ae36baf1c5c5ce5c3a5920280d8
BCrawfordScott <briancrawfordscott@gmail.com>
2021-12-05 17:46:59 -0800

    Add test frameworks

---
+++ lib/aoc_2021/day6/day6.ex
@@ -0,0 +1,13 @@
+defmodule Aoc2021.Day6 do
+  @input_file Path.dirname(__ENV__.file) <> "/input.txt"
+
+  def part1, do: part1(@input_file)
+  def part1(path) do
+    path
+  end
+
+  def part2, do: part2(@input_file)
+  def part2(path) do
+    path
+  end
+end
---
+++ test/aoc_2021/day6/day6_test.exs
@@ -0,0 +1,24 @@
+defmodule Aoc2021.Day6Test do
+  use ExUnit.Case
+  doctest Aoc2021.Day6
+
+  import Aoc2021.Day6, only: [
+    part1: 1,
+    part2: 1,
+  ]
+
+  # @test_file Path.dirname(__ENV__.file) <> "/test.txt"
+
+  @tag skip: "Not yet implemented"
+  test "has an input file" do
+    # assert(File.exists?(File.cwd! <> "/lib/aoc_2021/day6/input.txt"))
+  end
+  @tag skip: "Not yet implemented"
+  test "part 1 delivers the right output" do
+    # assert(part1(@test_file) == :none)
+  end
+  @tag skip: "Not yet implemented"
+  test "part 2 delivers the right output" do
+    # assert(part2(@test_file) == :none)
+  end
+end
